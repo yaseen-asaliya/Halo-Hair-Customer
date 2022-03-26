5c78fe40031e5c8b86d04767969baa783047cbfb
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-03-26 16:30:51 +0300

    Add files via upload

---
+++ Halo_Hair_customer/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Halo_Hair_customer.ViewModels;
+using Halo_Hair_customer.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Halo_Hair_customer
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+    
+        }
+
+    }
+}

8795d346e88d608c501ebb661fe2f1c20d846007
yazeed-dyriah <79354827+yazeed-dyriah@users.noreply.github.com>
2022-05-03 12:29:15 +0300

    sign in , sign up and reset password

--- customer app/customer app.Android/obj/Debug/120/android/src/androidx/recyclerview/R.java
+++ customer app/customer app.Android/obj/Debug/120/android/src/androidx/recyclerview/R.java
@@ -70,18 +70,18 @@ public final class R {
 		public static final int notification_top_pad_large_text = 0x7f060194;
 	}
 	public static final class drawable {
-		public static final int notification_action_background = 0x7f0700bd;
-		public static final int notification_bg = 0x7f0700be;
-		public static final int notification_bg_low = 0x7f0700bf;
-		public static final int notification_bg_low_normal = 0x7f0700c0;
-		public static final int notification_bg_low_pressed = 0x7f0700c1;
-		public static final int notification_bg_normal = 0x7f0700c2;
-		public static final int notification_bg_normal_pressed = 0x7f0700c3;
-		public static final int notification_icon_background = 0x7f0700c4;
-		public static final int notification_template_icon_bg = 0x7f0700c5;
-		public static final int notification_template_icon_low_bg = 0x7f0700c6;
-		public static final int notification_tile_bg = 0x7f0700c7;
-		public static final int notify_panel_notification_icon_bg = 0x7f0700c8;
+		public static final int notification_action_background = 0x7f0700be;
+		public static final int notification_bg = 0x7f0700bf;
+		public static final int notification_bg_low = 0x7f0700c0;
+		public static final int notification_bg_low_normal = 0x7f0700c1;
+		public static final int notification_bg_low_pressed = 0x7f0700c2;
+		public static final int notification_bg_normal = 0x7f0700c3;
+		public static final int notification_bg_normal_pressed = 0x7f0700c4;
+		public static final int notification_icon_background = 0x7f0700c5;
+		public static final int notification_template_icon_bg = 0x7f0700c6;
+		public static final int notification_template_icon_low_bg = 0x7f0700c7;
+		public static final int notification_tile_bg = 0x7f0700c8;
+		public static final int notify_panel_notification_icon_bg = 0x7f0700c9;
 	}
 	public static final class id {
 		public static final int accessibility_action_clickable_span = 0x7f08000f;
